0500796ab59e62fe51d733fe75e1185ee99c2a72
Robby Fermawan <114131353+FatihHanunnah@users.noreply.github.com>
2023-06-10 13:04:58 +0700

    Update penjualan-list-customer.php

--- app/Views/themes/modern/penjualan-list-customer.php
+++ app/Views/themes/modern/penjualan-list-customer.php
@@ -1,18 +1,14 @@
-<?php 
+<?php
 
-$column =[
-			'ignore_urut' => 'No'
-			, 'nama_customer' => 'Nama'
-			, 'alamat_customer' => 'Alamat'
-			, 'no_telp' => 'No. Tlp'
-			, 'ignore_pilih' => 'Pilih'
-		];
+$column = [
+	'ignore_urut' => 'No', 'nama_customer' => 'Nama', 'alamat_customer' => 'Alamat', 'no_telp' => 'No. Tlp','jenisharga' => 'JenisHarga', 'ignore_pilih' => 'Pilih'
+];
 
-$settings['order'] = [1,'asc'];
+$settings['order'] = [1, 'asc'];
 $index = 0;
 $th = '';
 foreach ($column as $key => $val) {
-	$th .= '<th>' . $val . '</th>'; 
+	$th .= '<th>' . $val . '</th>';
 	if (strpos($key, 'ignore') !== false) {
 		$settings['columnDefs'][] = ["targets" => $index, "orderable" => false];
 	}
@@ -22,26 +18,26 @@ foreach ($column as $key => $val) {
 ?>
 
 <table id="jwdmodal-table-result" class="table display table-striped table-bordered table-hover" style="width:100%">
-<thead>
-	<tr>
-		<?=$th?>
-	</tr>
-</thead>
-<tfoot>
-	<tr>
-		<?=$th?>
-	</tr>
-</tfoot>
+	<thead>
+		<tr>
+			<?= $th ?>
+		</tr>
+	</thead>
+	<tfoot>
+		<tr>
+			<?= $th ?>
+		</tr>
+	</tfoot>
 </table>
 <?php
-	foreach ($column as $key => $val) {
-		$column_dt[] = ['data' => $key];
-	}
+foreach ($column as $key => $val) {
+	$column_dt[] = ['data' => $key];
+}
 $id_lokasi_usaha = '';
 if (!empty($_GET['id_lokasi_usaha'])) {
 	$id_lokasi_usaha = '?id_lokasi_usaha=' . $_GET['id_lokasi_usaha'];
 }
 ?>
-<span id="jwdmodal-dataTables-column" style="display:none"><?=json_encode($column_dt)?></span>
-<span id="jwdmodal-dataTables-setting" style="display:none"><?=json_encode($settings)?></span>
-<span id="jwdmodal-dataTables-url" style="display:none"><?=base_url() . '/penjualan/getDataDTCustomer'?></span>
+<span id="jwdmodal-dataTables-column" style="display:none"><?= json_encode($column_dt) ?></span>
+<span id="jwdmodal-dataTables-setting" style="display:none"><?= json_encode($settings) ?></span>
+<span id="jwdmodal-dataTables-url" style="display:none"><?= base_url() . '/penjualan/getDataDTCustomer' ?></span>
