a4950e8845745ef65965b52d0e9b47520c41784b
Robby Fermawan <114131353+FatihHanunnah@users.noreply.github.com>
2023-06-10 13:03:35 +0700

    Update customer-form.php

--- app/Views/themes/modern/customer-form.php
+++ app/Views/themes/modern/customer-form.php
@@ -1,24 +1,26 @@
 <div class="card">
 	<div class="card-header">
-		<h5 class="card-title"><?=$title?></h5>
+		<h5 class="card-title"><?= $title ?></h5>
 	</div>
-	
+
 	<div class="card-body">
-		<?php 
-			helper ('html');
-			echo btn_link(['attr' => ['class' => 'btn btn-success btn-xs'],
-				'url' => $config->baseURL . $current_module['nama_module'] . '/add',
-				'icon' => 'fa fa-plus',
-				'label' => 'Tambah Data'
-			]);
-			
-			echo btn_link(['attr' => ['class' => 'btn btn-light btn-xs'],
-				'url' => $config->baseURL . $current_module['nama_module'],
-				'icon' => 'fa fa-arrow-circle-left',
-				'label' => $current_module['judul_module']
-			]);
+		<?php
+		helper('html');
+		echo btn_link([
+			'attr' => ['class' => 'btn btn-success btn-xs'],
+			'url' => $config->baseURL . $current_module['nama_module'] . '/add',
+			'icon' => 'fa fa-plus',
+			'label' => 'Tambah Data'
+		]);
+
+		echo btn_link([
+			'attr' => ['class' => 'btn btn-light btn-xs'],
+			'url' => $config->baseURL . $current_module['nama_module'],
+			'icon' => 'fa fa-arrow-circle-left',
+			'label' => $current_module['judul_module']
+		]);
 		?>
-		<hr/>
+		<hr />
 		<?php
 
 		if (!empty($message)) {
@@ -30,49 +32,91 @@
 				<div class="form-group row mb-3">
 					<label class="col-sm-3 col-md-2 col-lg-3 col-xl-2 col-form-label">Nama Customer</label>
 					<div class="col-sm-5">
-						<input class="form-control" type="text" name="nama_customer" value="<?=set_value('nama_customer', @$result['nama_customer'])?>" required="required"/>
+						<input class="form-control" type="text" name="nama_customer" value="<?= set_value('nama_customer', @$result['nama_customer']) ?>" required="required" />
 					</div>
 				</div>
 				<div class="form-group row mb-3">
 					<label class="col-sm-3 col-md-2 col-lg-3 col-xl-2 col-form-label">Alamat</label>
 					<div class="col-sm-5">
-						<textarea class="form-control" name="alamat_customer" required><?=set_value('alamat_customer', @$result['alamat_customer'])?></textarea>
+						<textarea class="form-control" name="alamat_customer" required><?= set_value('alamat_customer', @$result['alamat_customer']) ?></textarea>
 					</div>
 				</div>
 				<div class="form-group row mb-3">
 					<label class="col-sm-3 col-md-2 col-lg-3 col-xl-2 col-form-label">No. Telp</label>
 					<div class="col-sm-5">
-						<input class="form-control" type="text" name="no_telp" value="<?=set_value('no_telp', @$result['no_telp'])?>" required="required"/>
+						<input class="form-control" type="text" name="no_telp" value="<?= set_value('no_telp', @$result['no_telp']) ?>" required="required" />
 					</div>
 				</div>
 				<div class="form-group row mb-3">
 					<label class="col-sm-3 col-md-2 col-lg-3 col-xl-2 col-form-label">Email</label>
 					<div class="col-sm-5">
-						<input class="form-control" type="email" name="email" value="<?=set_value('email', @$result['email'])?>" required="required"/>
+						<input class="form-control" type="email" name="email" value="<?= set_value('email', @$result['email']) ?>" required="required" />
 					</div>
 				</div>
 				<div class="form-group row mb-3">
 					<label class="col-sm-3 col-md-2 col-lg-3 col-xl-2 col-form-label">Propinsi</label>
 					<div class="col-sm-5">
-						<?=options(['name' => 'id_wilayah_propinsi', 'class' => 'propinsi select2'], $propinsi, set_value('id_wilayah_propinsi', @$result['id_wilayah_propinsi'] ?: $default_propinsi) )?>
+						<?= options(['name' => 'id_wilayah_propinsi', 'class' => 'propinsi select2'], $propinsi, set_value('id_wilayah_propinsi', @$result['id_wilayah_propinsi'] ?: $default_propinsi)) ?>
 					</div>
 				</div>
 				<div class="form-group row mb-3">
 					<label class="col-sm-3 col-md-2 col-lg-3 col-xl-2 col-form-label">Kabupaten</label>
 					<div class="col-sm-5">
-						<?=options(['name' => 'id_wilayah_kabupaten', 'class' => 'kabupaten select2'], $kabupaten, set_value('id_wilayah_kabupaten', @$result['id_wilayah_kabupaten'] ?: $default_kabupaten))?>
+						<?= options(['name' => 'id_wilayah_kabupaten', 'class' => 'kabupaten select2'], $kabupaten, set_value('id_wilayah_kabupaten', @$result['id_wilayah_kabupaten'] ?: $default_kabupaten)) ?>
 					</div>
 				</div>
 				<div class="form-group row mb-3">
 					<label class="col-sm-3 col-md-2 col-lg-3 col-xl-2 col-form-label">Kecamatan</label>
 					<div class="col-sm-5">
-						<?=options(['name' => 'id_wilayah_kecamatan', 'class' => 'kecamatan select2'], $kecamatan, set_value('id_wilayah_kecamatan', @$result['id_wilayah_kecamatan'] ?: $default_kecamatan))?>
+						<?= options(['name' => 'id_wilayah_kecamatan', 'class' => 'kecamatan select2'], $kecamatan, set_value('id_wilayah_kecamatan', @$result['id_wilayah_kecamatan'] ?: $default_kecamatan)) ?>
 					</div>
 				</div>
 				<div class="form-group row mb-3">
 					<label class="col-sm-3 col-md-2 col-lg-3 col-xl-2 col-form-label">Kelurahan</label>
 					<div class="col-sm-5" style="position:relative">
-						<?=options(['name' => 'id_wilayah_kelurahan', 'class' => 'kelurahan select2'], $kelurahan, set_value('id_wilayah_kelurahan', @$result['id_wilayah_kelurahan'] ?: $default_kelurahan))?>
+						<?= options(['name' => 'id_wilayah_kelurahan', 'class' => 'kelurahan select2'], $kelurahan, set_value('id_wilayah_kelurahan', @$result['id_wilayah_kelurahan'] ?: $default_kelurahan)) ?>
+					</div>
+				</div>
+				<div class="form-group row mb-3">
+					<label class="col-sm-3 col-md-2 col-lg-3 col-xl-2 col-form-label">Nama Sales</label>
+					<div class="col-sm-5" style="position:relative">
+						<select style="width: 100%;" name="id_sales" class="sales select2">
+							<?php
+							foreach ($data_sales as $sales) {
+								if (@$result['id_sales']) {
+									if ($result['id_sales'] == $sales['id_user']) {
+										$s = "selected";
+									} else {
+										$s = "";
+									}
+								} else {
+									$s = "";
+								}
+								echo '<option nama_sales="' . $sales['nama'] . '" ' . $s . ' value="' . $sales['id_user'] . '">' . $sales['nama'] . '</option>';
+							} ?>
+						</select>
+						<input type="hidden" name="nama_sales" id="nama_sales" />
+					</div>
+				</div>
+				<div class="form-group row mb-3">
+					<label class="col-sm-3 col-md-2 col-lg-3 col-xl-2 col-form-label">Jenis Harga</label>
+					<div class="col-sm-5" style="position:relative">
+						<select style="width: 100%;" name="id_jenis_harga" class="jenis_harga select2">
+							<?php $sl = "";
+							foreach ($jenis_harga as $harga) {
+								if (@$result['id_jenis_harga']) {
+									if ($result['id_jenis_harga'] == $harga['id_jenis_harga']) {
+										$sl = "selected";
+									} else {
+										$sl = "";
+									}
+								} else {
+									$sl = "";
+								}
+								echo '<option nama_jenis="' . $harga['nama_jenis_harga'] . '" ' . $sl . ' value="' . $harga['id_jenis_harga'] . '">' . $harga['nama_jenis_harga'] . '</option>';
+							} ?>
+						</select>
+						<input type="hidden" name="nama_jenis_harga" id="nama_jenis_harga" />
 					</div>
 				</div>
 				<div class="form-group row mb-3">
@@ -80,8 +124,7 @@
 					<div class="col-sm-5">
 						<?php
 						$foto = @$result['foto'];
-						if (!empty($foto) ) 
-						{
+						if (!empty($foto)) {
 							$note = '';
 							if (file_exists(ROOTPATH . 'public/images/foto/' . $foto)) {
 								$image = $config->baseURL . 'public/images/foto/' . $foto;
@@ -91,26 +134,26 @@
 							}
 							echo '<div class="img-choose" style="margin:inherit;margin-bottom:10px">
 									<div class="img-choose-container">
-										<img src="'. $image . '?r=' . time() . '"/>
+										<img src="' . $image . '?r=' . time() . '"/>
 										<a href="javascript:void(0)" class="remove-img"><i class="fas fa-times"></i></a>
 									</div>
 								</div>
-								' . $note .'
+								' . $note . '
 								';
 						}
 						?>
 						<input type="hidden" class="foto-delete-img" name="foto_delete_img" value="0">
-						<input type="hidden" class="foto-max-size" name="foto_max_size" value="300000"/>
+						<input type="hidden" class="foto-max-size" name="foto_max_size" value="300000" />
 						<input type="file" class="file form-control" name="foto">
-							<?php if (!empty($form_errors['foto'])) echo '<small class="alert alert-danger">' . $form_errors['foto'] . '</small>'?>
-							<small class="small" style="display:block">Maksimal 300Kb, Minimal 100px x 100px, Tipe file: .JPG, .JPEG, .PNG</small>
+						<?php if (!empty($form_errors['foto'])) echo '<small class="alert alert-danger">' . $form_errors['foto'] . '</small>' ?>
+						<small class="small" style="display:block">Maksimal 300Kb, Minimal 100px x 100px, Tipe file: .JPG, .JPEG, .PNG</small>
 						<div class="upload-img-thumb"><span class="img-prop"></span></div>
 					</div>
 				</div>
 				<div class="form-group row mb-0">
 					<div class="col-sm-5">
 						<button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
-						<input type="hidden" name="id" value="<?=@$_GET['id']?>"/>
+						<input type="hidden" name="id" value="<?= @$_GET['id'] ?>" />
 					</div>
 				</div>
 			</div>
