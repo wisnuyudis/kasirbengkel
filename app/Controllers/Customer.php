67f94ad9a2399eb427a7c77d1a9dfaebb5940dfc
Robby Fermawan <114131353+FatihHanunnah@users.noreply.github.com>
2023-06-10 12:57:04 +0700

    Update Customer.php

--- app/Controllers/Customer.php
+++ app/Controllers/Customer.php
@@ -1,47 +1,49 @@
 <?php
+
 /**
-*	App Name	: Aplikasi Kasir Berbasis Web	
-*	Developed by: Agus Prawoto Hadi
-*	Website		: https://jagowebdev.com
-*	Year		: 2022
-*/
+ *	App Name	: Aplikasi Kasir Berbasis Web	
+ *	Developed by: Agus Prawoto Hadi
+ *	Website		: https://jagowebdev.com
+ *	Year		: 2022
+ */
 
 namespace App\Controllers;
+
 use App\Models\CustomerModel;
 use App\Models\WilayahModel;
 
 class Customer extends \App\Controllers\BaseController
 {
-	public function __construct() {
-		
+	public function __construct()
+	{
+
 		parent::__construct();
-		
-		$this->model = new CustomerModel;	
+
+		$this->model = new CustomerModel;
 		$this->data['site_title'] = 'Data Customer';
-		
-		$this->addJs ( $this->config->baseURL . 'public/vendors/bootstrap-datepicker/js/bootstrap-datepicker.js' );
-		$this->addJs ( $this->config->baseURL . 'public/themes/modern/js/date-picker.js');
-		$this->addJs ( $this->config->baseURL . 'public/themes/modern/js/image-upload.js');
+
+		$this->addJs($this->config->baseURL . 'public/vendors/bootstrap-datepicker/js/bootstrap-datepicker.js');
+		$this->addJs($this->config->baseURL . 'public/themes/modern/js/date-picker.js');
+		$this->addJs($this->config->baseURL . 'public/themes/modern/js/image-upload.js');
 		// $this->addJs ( $this->config->baseURL . 'public/vendors/datatables/datatables.min.js');
 		// $this->addStyle ( $this->config->baseURL . 'public/vendors/datatables/datatables.min.css');
 		// $this->addJs ( $this->config->baseURL . 'public/themes/modern/js/data-tables-ajax.js');
-		$this->addJs ( $this->config->baseURL . 'public/themes/modern/js/customer.js');
-		$this->addStyle ( $this->config->baseURL . 'public/vendors/bootstrap-datepicker/css/bootstrap-datepicker3.css');
-		
-		$this->addJs ( $this->config->baseURL . 'public/vendors/jquery.select2/js/select2.full.min.js' );
-		$this->addStyle ( $this->config->baseURL . 'public/vendors/jquery.select2/css/select2.min.css' );
-		$this->addStyle ( $this->config->baseURL . 'public/vendors/jquery.select2/bootstrap-5-theme/select2-bootstrap-5-theme.min.css' );
+		$this->addJs($this->config->baseURL . 'public/themes/modern/js/customer.js');
+		$this->addStyle($this->config->baseURL . 'public/vendors/bootstrap-datepicker/css/bootstrap-datepicker3.css');
+
+		$this->addJs($this->config->baseURL . 'public/vendors/jquery.select2/js/select2.full.min.js');
+		$this->addStyle($this->config->baseURL . 'public/vendors/jquery.select2/css/select2.min.css');
+		$this->addStyle($this->config->baseURL . 'public/vendors/jquery.select2/bootstrap-5-theme/select2-bootstrap-5-theme.min.css');
 	}
-	
+
 	public function index()
 	{
 		$this->hasPermissionPrefix('read');
-		
+
 		$data = $this->data;
-		if (!empty($_POST['delete'])) 
-		{
+		if (!empty($_POST['delete'])) {
 			$this->hasPermissionPrefix('delete', 'dokter');
-			
+
 			$result = $this->model->deleteData();
 			// $result = true;
 			if ($result) {
@@ -52,19 +54,23 @@ class Customer extends \App\Controllers\BaseController
 		}
 		$this->view('customer-result.php', $data);
 	}
-	
-	private function setEdit() {
+
+	private function setEdit()
+	{
 		$this->data['title'] = 'Edit Data Customer';
 		$this->data['breadcrumb']['Edit'] = '';
 		unset($this->data['breadcrumb']['Add']);
 	}
-	
-	public function add() 
+
+	public function add()
 	{
 		$wilayah = new \App\Controllers\Wilayah();
 		$data_wilayah = $wilayah->getDataWilayah();
-		$this->data = array_merge($this->data, $data_wilayah);
-		
+		$user['data_sales'] = $this->model->getuser();
+		$user['jenis_harga'] = $this->model->getjenisharga();
+		$user['cekrole'] = $this->model->cekrole();
+		$this->data = array_merge($this->data, $data_wilayah, $user);
+
 		if (!empty($_POST['id'])) {
 			$this->setEdit();
 		} else {
@@ -75,29 +81,28 @@ class Customer extends \App\Controllers\BaseController
 
 		$this->data['message'] = [];
 
-		if (isset($_POST['submit'])) 
-		{
+		if (isset($_POST['submit'])) {
 			$form_errors = $this->validateForm();
-							
+
 			if ($form_errors) {
 				$this->data['message']['status'] = 'error';
 				$this->data['message']['message'] = $form_errors;
 			} else {
-				
+
 				$this->data['message'] = $this->model->saveData();
 				$this->setEdit();
 			}
 		}
-		
-		if (@$_GET['ajax'] ) 
-		{
+
+		if (@$_GET['ajax']) {
 			if (@$_POST['submit']) {
 				$result = $this->data['message'];
 				if ($result['status'] == 'ok') {
-					$detail_customer = $this->model->getCustomerById( $this->data['message']['id_customer'] );
+					$detail_customer = $this->model->getCustomerById($this->data['message']['id_customer']);
 					$result['customer'] = $detail_customer;
 				}
-				echo json_encode($result); exit;
+				echo json_encode($result);
+				exit;
 			} else {
 				echo view('themes/modern/customer-form-ajax.php', $this->data);
 			}
@@ -105,89 +110,90 @@ class Customer extends \App\Controllers\BaseController
 			$this->view('customer-form.php', $this->data);
 		}
 	}
-	
+
 	public function edit()
 	{
-		$this->hasPermissionPrefix('update', 'customer');
 		
+		$this->hasPermissionPrefix('update', 'customer');
+
 		$this->data['title'] = 'Edit Customer';
 		$this->data['breadcrumb']['Edit'] = '';
-		
+
 		if (empty($_GET['id'])) {
 			$this->errorDataNotFound();
 		}
-		
+
 		$this->data['message'] = [];
-		if (isset($_POST['submit'])) 
-		{
+		if (isset($_POST['submit'])) {
 			$form_errors = $this->validateForm();
-							
+
 			if ($form_errors) {
 				$this->data['message']['status'] = 'error';
 				$this->data['message']['content'] = $form_errors;
 			} else {
-				
+
 				$message = $this->model->saveData();
 				$this->data = array_merge($this->data, $message);
 			}
 		}
-		
+
 		$id = !empty($_POST['id']) ? $_POST['id'] : $_GET['id'];
 		$this->data['id'] = $id;
 		$this->data['result'] = $this->model->getCustomerById($id);
-		
+
 		if (empty($this->data['result'])) {
 			$this->errorDataNotFound();
 		}
-	
+
 		$wilayah = new \App\Controllers\Wilayah();
 		$data_wilayah = $wilayah->getDataWilayah($this->data['result']['id_wilayah_kelurahan']);
-		$this->data = array_merge($this->data, $data_wilayah);
-		
+		$user['data_sales'] = $this->model->getuser();
+		$user['jenis_harga'] = $this->model->getjenisharga();
+		$this->data = array_merge($this->data, $data_wilayah, $user);
+
 		$this->view('customer-form.php', $this->data);
 	}
-	
-	private function validateForm($check_unique = false) {
-	
+
+	private function validateForm($check_unique = false)
+	{
+
 		$validation =  \Config\Services::validation();
 		$validation->setRule('nama_customer', 'Nama Customer', 'trim|required');
 		$validation->setRule('alamat_customer', 'Alamat Customer', 'trim|required');
 		$validation->withRequest($this->request)->run();
 		$form_errors = $validation->getErrors();
-		
+
 		return $form_errors;
 	}
-	
-	public function getDataDT() {
-		
+
+	public function getDataDT()
+	{
+
 		$this->hasPermissionPrefix('read');
-		
-		$num_data = $this->model->countAllData( $this->whereOwn() );
+
+		$num_data = $this->model->countAllData($this->whereOwn());
 		$result['draw'] = $start = $this->request->getPost('draw') ?: 1;
 		$result['recordsTotal'] = $num_data;
-		
-		$query = $this->model->getListData( $this->whereOwn() );
+
+		$query = $this->model->getListData($this->whereOwn());
 		$result['recordsFiltered'] = $query['total_filtered'];
-				
+
 		helper('html');
 		$id_user = $this->session->get('user')['id_user'];
-		
+
 		$no = $this->request->getPost('start') + 1 ?: 1;
-		foreach ($query['data'] as $key => &$val) 
-		{
+		foreach ($query['data'] as $key => &$val) {
 			$val['ignore_search_urut'] = $no;
 			$val['ignore_search_action'] = btn_action([
-									'edit' => ['url' => $this->config->baseURL . $this->currentModule['nama_module'] . '/edit?id='. $val['id_customer']]
-								, 'delete' => ['url' => ''
-												, 'id' =>  $val['id_customer']
-												, 'delete-title' => 'Hapus data dokter: <strong>'.$val['nama_customer'].'</strong> ?'
-											]
-							]);
+				'edit' => ['url' => $this->config->baseURL . $this->currentModule['nama_module'] . '/edit?id=' . $val['id_customer']], 'delete' => [
+					'url' => '', 'id' =>  $val['id_customer'], 'delete-title' => 'Hapus data dokter: <strong>' . $val['nama_customer'] . '</strong> ?'
+				]
+			]);
 			$no++;
 		}
-					
+
 		$result['data'] = $query['data'];
-		echo json_encode($result); exit();
+		echo json_encode($result);
+		exit();
 	}
-	
 }
